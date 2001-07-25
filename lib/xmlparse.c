7dd16f3040b051b368ebf4941633e95bb530546e
fdrake <fdrake>
2001-07-25 03:29:56 +0000

    Revert unintended portion of a checkin; these files were not supposed to have been changed with the most recent Makefile changes.

--- lib/xmlparse.c
+++ lib/xmlparse.c
@@ -371,7 +371,6 @@ typedef struct {
   XML_EndNamespaceDeclHandler m_endNamespaceDeclHandler;
   XML_NotStandaloneHandler m_notStandaloneHandler;
   XML_ExternalEntityRefHandler m_externalEntityRefHandler;
-  XML_InternalEntityRefHandler m_internalEntityRefHandler;
   void *m_externalEntityRefHandlerArg;
   XML_UnknownEncodingHandler m_unknownEncodingHandler;
   XML_ElementDeclHandler m_elementDeclHandler;
@@ -721,7 +720,6 @@ XML_Parser XML_ExternalEntityParserCreate(XML_Parser oldParser,
   XML_EndNamespaceDeclHandler oldEndNamespaceDeclHandler = endNamespaceDeclHandler;
   XML_NotStandaloneHandler oldNotStandaloneHandler = notStandaloneHandler;
   XML_ExternalEntityRefHandler oldExternalEntityRefHandler = externalEntityRefHandler;
-  XML_InternalEntityRefHandler oldInternalEntityRefHandler = internalEntityRefHandler;
   XML_UnknownEncodingHandler oldUnknownEncodingHandler = unknownEncodingHandler;
   XML_ElementDeclHandler oldElementDeclHandler = elementDeclHandler;
   XML_AttlistDeclHandler oldAttlistDeclHandler = attlistDeclHandler;
@@ -1039,12 +1037,6 @@ void XML_SetExternalEntityRefHandlerArg(XML_Parser parser, void *arg)
     externalEntityRefHandlerArg = arg;
   else
     externalEntityRefHandlerArg = parser;
-}
-
-void XML_SetInternalEntityRefHandler(XML_Parser parser,
-				     XML_InternalEntityRefHandler handler)
-{
-  internalEntityRefHandler = handler;
 }
 
 void XML_SetUnknownEncodingHandler(XML_Parser parser,
