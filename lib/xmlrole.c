acef8295392a56efd3be5205f935a0d8bb45d52c
gstein <gstein>
2002-05-17 21:30:06 +0000

    Various configuration fixes, focused primarily around expat_config.h (which used to be called config.h)
    * configure.in:
      - change the header name to expat_config.h to prevent any possible
        conflict with some other packages 'config.h'
      - add a prerequisite on autoconf 2.50 or newer
      - shift some acconfig.h values to AC_DEFINE() values
    
    * MANIFEST:
      - rename config.h.in to expat_config.h.in
    
    * Makefile.in:
      - clean out expat_config.h* instead of config.h*
    
    * acconfig.h:
      - remove the XML_NS, XML_DTD, and XML_CONTEXT_BYTES symbols; they
        are now defined directly by configure.in
      - shift the memmove() compatibility to xmlparse.c, its only consumer
      - remove the @BOTTOM@ part since there are no more values to put at
        the bottom of expat_config.h.in via acconfig.h
    
    * xmlparse.c:
      - switch from config.h to expat_config.h
      - insert macro to define memmove() if it isn't available, but
        bcopy() is available. also, changed one branch to #error rather
        than insert bogus code.
    
    * xmlrole.c:
      - tweak the copyright header, per Fred-ism
      - remove the unused RCSId[] variable (no other file has these)
      - switch from config.h to expat_config.h
    
    * xmltok.c:
      - switch from config.h to expat_config.h

--- lib/xmlrole.c
+++ lib/xmlrole.c
@@ -1,15 +1,11 @@
-/*
-Copyright (c) 1998, 1999 Thai Open Source Software Center Ltd
-See the file COPYING for copying permission.
+/* Copyright (c) 1998, 1999 Thai Open Source Software Center Ltd
+   See the file COPYING for copying permission.
 */
-
-static char RCSId[]
-  = "$Header$";
 
 #ifdef COMPILED_FROM_DSP
 #  include "winconfig.h"
 #else
-#  include <config.h>
+#  include <expat_config.h>
 #endif /* ndef COMPILED_FROM_DSP */
 
 #include "xmlrole.h"
